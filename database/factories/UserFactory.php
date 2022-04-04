bf5d9fbe926659bfd2d6904141e5a8fb5b471302
Jan Ruhfus <janruhfus0@gmail.com>
2022-04-04 15:19:27 +0200

    Create users

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\School;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $school = School::factory(1)->create()->first();
+
+        return [
+            'school_id' => $school->id
+        ];
+    }
+}
