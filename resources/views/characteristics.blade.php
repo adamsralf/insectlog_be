e2f1b7eba875925d3b57f6e7771e1eb9189e5d14
Jan Ruhfus <janruhfus0@gmail.com>
2022-10-28 13:16:10 +0200

    Change design of profile

--- resources/views/characteristics.blade.php
+++ resources/views/characteristics.blade.php
@@ -17,55 +17,25 @@
     <h1 class="text-center text-4xl font-bold mb-4">{{ $insect->species_name }}</h1>
 
     <p class="mb-16">{{ $insect->description }}</p>
-    <span class="right">
-        <img class="mb-4" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/652/unsplash-daisy.jpg" alt="daisy photographed from below">
-        <br>
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/652/unsplash-daisy.jpg" alt="daisy photographed from below">
-    </span>
 
-    <table width="468" cellspacing="0" cellpadding="0" border="0" style="width: 468px; height: 201px;">
-        <tbody>
-            <tr>
-                <td>Größe:</td>
-                <td>{{ $info->size }}</td>
-            </tr>
+    <div class="grid grid-cols-2 gap-1 justify-evenly">
+        <div>
+            <p><b>Größe:</b> {{ $info->size }}</p>
+            <p><b>Lebensdauer:</b> {{ $info->lifespan }}</p>
+            <p><b>Ernährung:</b> {{ $info->food }}</p>
+            <p><b>Gattung:</b> {{ $info->species }}</p>
+            <p><b>Wissenschaftlicher Name:</b> {{ $info->species_name }}</p>
+            <p><b>Lebensraum:</b> {{ $info->habitat }}</p>
+            <p><b>Feinde:</b> {{ $info->enemies }}</p>
+            <p><b>Merkmale:</b> {{ $info->indicator }}</p>
+        </div>
 
-            <tr>
-                <td>Lebensdauer:</td>
-                <td>{{ $info->lifespan }}</td>
-            </tr>
-
-            <tr>
-                <td>Ernährung:</td>
-                <td>{{ $info->food }}</td>
-            </tr>
-
-            <tr>
-                <td>Gattung:</td>
-                <td>{{ $info->species }}</td>
-            </tr>
-
-            <tr>
-                <td>Wissenschaftlicher Name:</td>
-                <td>{{ $info->species_name }}</td>
-            </tr>
-
-            <tr>
-                <td>Lebensraum:</td>
-                <td>{{ $info->habitat }}</td>
-            </tr>
-
-            <tr>
-                <td>Feinde:</td>
-                <td>{{ $info->enemies }}</td>
-            </tr>
-
-            <tr>
-                <td>Merkmale:</td>
-                <td>{{ $info->indicator }}</td>
-            </tr>
-        </tbody>
-    </table>
+        <div>
+            <img class="mb-4" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/652/unsplash-daisy.jpg" alt="daisy photographed from below">
+            <br>
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/652/unsplash-daisy.jpg" alt="daisy photographed from below">
+        </div>
+    </div>
 
     <div id="qr"><img src="https://via.placeholder.com/150" height="100%" width="100%"></div>
 </body>
